3c4b005c48f4c61462dc4d99cb1fcd3f90e9374c
Saac Sornchai <saacsos@gmail.com>
2019-06-01 16:57:21 +0700

    config validator

--- start/app.js
+++ start/app.js
@@ -19,7 +19,8 @@ const providers = [
   '@adonisjs/shield/providers/ShieldProvider',
   '@adonisjs/session/providers/SessionProvider',
   '@adonisjs/auth/providers/AuthProvider',
-  '@radmen/adonis-lucid-soft-deletes/providers/SoftDeletesProvider'
+  '@radmen/adonis-lucid-soft-deletes/providers/SoftDeletesProvider',
+  '@adonisjs/validator/providers/ValidatorProvider'
 ]
 
 /*
