2f43db4b76b06f6299a1d3d9a28722aceea66272
Saac Sornchai <saacsos@gmail.com>
2019-06-01 16:53:33 +0700

    new TaskController and route

--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,6 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+Route.get('tasks', 'TaskController.index')
+// Route.post('tasks', 'TaskController.store')
+// Route.delete('tasks/:id', 'TaskController.destroy')
