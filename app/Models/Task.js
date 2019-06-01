76fa8f187f6e0f52174a4777818b9f56af14a9f7
Saac Sornchai <saacsos@gmail.com>
2019-06-01 16:39:52 +0700

    new model Task

---
+++ app/Models/Task.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Task extends Model {
+    static boot () {
+        super.boot()
+
+        this.addTrait('@provider:Lucid/SoftDeletes')
+    }
+}
+
+module.exports = Task
