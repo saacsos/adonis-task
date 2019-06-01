1d074dd202d29052115e46c74ef1c08d864327af
Saac Sornchai <saacsos@gmail.com>
2019-06-01 17:12:36 +0700

    Task.store implementation and create form

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -2,12 +2,38 @@
 
 // reference Task model
 const Task = use('App/Models/Task')
+// reference Validator
+const { validate } = use('Validator')
 
 class TaskController {
     async index({ view }) {
         const tasks = await Task.all()
 
         return view.render('tasks.index', { tasks: tasks.toJSON() })
+    }
+
+    async store({ request, response, session }) {
+        const validation = await validate(request.all(), {
+            title: 'required|min:3|max:255',
+            detail: 'max:512'
+        })
+
+        if (validation.fails()) {
+            session.withErrors(validation.messages())
+                    .flashAll()
+            return response.redirect('back')
+        }
+
+        const task = new Task()
+        task.title = request.input('title')
+        task.detail = request.input('detail')
+        await task.save()
+
+        session.flash({
+            notification: 'Task ' + '"' + task.title + '" added!'
+        })
+
+        return response.redirect('back')
     }
 }
 
